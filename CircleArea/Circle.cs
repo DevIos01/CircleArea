fef4b7b8f9e13a4247288f491619f39568d4a897
Shubham Panth <GamingCraft52@gmail.com>
2026-09-25 11:02:46 +0200

    Add Circle class with a radius constructor

---
+++ CircleArea/Circle.cs
@@ -0,0 +1,13 @@
+namespace CircleArea;
+
+class Circle
+{
+    // The radius of the circle
+    int _radius;
+
+    // Constructor, runs when a new Circle is created
+    public Circle(int radius)
+    {
+        _radius = radius;
+    }
+}
