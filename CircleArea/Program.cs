0c976bc86f6a59b6b8a11ebb7a2398efbb52c17e
Shubham Panth <GamingCraft52@gmail.com>
2026-09-25 11:12:27 +0200

    Create circles with radius 5 and 6 and print their areas

--- CircleArea/Program.cs
+++ CircleArea/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace CircleArea;
+namespace CircleArea;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Create two circles with different radius
+        Circle smallCircle = new Circle(5);
+        Circle bigCircle = new Circle(6);
+
+        // Print the area of each circle with two decimals
+        Console.WriteLine($"Arean på en cirkel med radie 5 är {smallCircle.GetArea():F2}");
+        Console.WriteLine($"Arean på en cirkel med radie 6 är {bigCircle.GetArea():F2}");
     }
 }
